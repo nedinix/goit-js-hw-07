78f37e4cd769bdb8fde269bc2f90c021a4e826e3
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-04-01 02:38:48 +0300

    task-2 without item-4

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery");
+container.addEventListener("click", onClick);
+
+const markup = galleryItems
+	.map(
+		({ preview, original, description }) =>
+			`<li class='gallery__item'>
+        <a class='gallery__link' href='${original}'>
+          <img
+            class='gallery__image'
+            src='${preview}'
+            data-source='${original}'
+            alt='${description}'
+          />
+        </a>
+      </li>`
+	)
+	.join("");
+
+container.insertAdjacentHTML("beforeend", markup);
+
+var imageEl = new SimpleLightbox(".gallery a", {
+	/* options */
+});
+
+function onClick(e) {
+	imageEl.show.simplelightbox;
+}
